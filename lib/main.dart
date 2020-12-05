693d882391b35d503bdca1e47789a8b9ca1905ec
jolan94 <jolan94@gmail.com>
2020-12-05 22:25:05 +0530

    Third

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Days Counter',
+      title: 'Days Counter!',
       theme: ThemeData(
           textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
           visualDensity: VisualDensity.adaptivePlatformDensity,
